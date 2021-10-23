6c72b3cac85290774d6d33cc6de6e1d62fdf0661
Ross Neilson <rossneilson99@gmail.com>
2021-10-23 21:42:05 +0100

    feat: Add dual maps to guild page

--- src/components/Guild/GuildMain.tsx
+++ src/components/Guild/GuildMain.tsx
@@ -1,32 +1,62 @@
 // Externals
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { UserInfo } from './guildComponents/UserInfo';
 
+import { generateMap } from '../../map-generator/index';
+import { addPlayerPos } from './guildComponents/MapUtil';
 
-const GuildWrap=styled.div`
-display:flex;
-flex-direction: column;
-`
+const GuildWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
 
+const MapWrap = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
+export const GuildMain: React.FC = () => {
+  const [originalMap, setOriginalMap] = useState('');
+  const [guildOneMap, setGuildOneMap] = useState('');
+  const [guildTwoMap, setGuildTwoMap] = useState('');
+  const [json, setJson] = useState(null);
+  const [guildOnePos, setGuildOnePos] = useState([0, 0]);
+  const [guildTwoPos, setGuildTwoPos] = useState([0, 0]);
 
-interface LinkedButtonsProps {
+  useEffect(() => {
+    const { svg, json } = generateMap();
+    let initialRoom = null;
+    json.map(room => {
+      if (room.roomNumber === 1) {
+        initialRoom = room;
+      }
+    });
 
-}
+    setOriginalMap(svg);
+    setGuildOneMap(addPlayerPos(svg, initialRoom));
+    setGuildTwoMap(addPlayerPos(svg, initialRoom));
 
-export const GuildMain: React.FC<LinkedButtonsProps> = () => {
+    setJson(json);
 
+    json.map(room => {
+      if (room.roomNumber === 1) {
+        setGuildOnePos([room.x, room.y]);
+        setGuildTwoPos([room.x, room.y]);
+      }
+    });
+  }, []);
+
+  console.log({ guildOnePos, guildTwoPos, originalMap, json });
 
   return (
     <GuildWrap>
-
-      {/*map goes here*/}
-      <div>Here</div>
-      <UserInfo/>
-
+      <MapWrap>
+        <div dangerouslySetInnerHTML={{ __html: guildOneMap }} />
+        <div dangerouslySetInnerHTML={{ __html: guildTwoMap }} />
+      </MapWrap>
+      <UserInfo />
     </GuildWrap>
-
-
   );
 };
