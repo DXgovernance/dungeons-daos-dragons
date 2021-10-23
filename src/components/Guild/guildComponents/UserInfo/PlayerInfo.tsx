c1b20f533d738df63baa1febdaf9154ac2716a43
My Violet <milanrasovic.f@gmail.com>
2021-10-23 14:43:42 +0100

    added health bar

--- src/components/Guild/guildComponents/UserInfo/PlayerInfo.tsx
+++ src/components/Guild/guildComponents/UserInfo/PlayerInfo.tsx
@@ -8,7 +8,19 @@ display: flex;
 flex-direction: column;
 grid-gap:2px;
 `
-
+export const Progress = styled.div<{ width: number; outcomeIndex?: number; selected?: boolean; color?: keyof Colors }>`
+  background-color:green;
+  border-radius: 4px;
+  height: 100%;
+  transition: width 0.25s ease-out, background-color 0.25s ease-out;
+  width: ${props => props.width}%;
+`
+const ProgressBar = styled.div<{ height?: number }>`
+  background-color: #f5f5f5;
+  border-radius: 4px;
+  height: ${props => (props.height ? props.height : 6)}px;
+  overflow: hidden;
+`
 
 interface LinkedButtonsProps {
 
@@ -19,6 +31,10 @@ export const PlayerInfo: React.FC<LinkedButtonsProps> = () => {
 
   return (
     <StyledWrapper>
+      <ProgressBar>
+        <Progress width={22} />
+      </ProgressBar>
+
       <Box>Health:22</Box>
       <Box>Score: 36</Box>
       <Box>Some: 12</Box>
